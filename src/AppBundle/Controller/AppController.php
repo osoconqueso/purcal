5c1f8d28a1dc2e484632ad55516d693357cfeb57
Jessica Hopper <jessica.hopper@gmail.com>
2016-07-27 02:29:14 +0000

    service and dropdown fragment

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -3,11 +3,20 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Model;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AppController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function getModels()
+    {
+       $models = $this->modelRepo->findAll();
+       return $models;
     }
 }
