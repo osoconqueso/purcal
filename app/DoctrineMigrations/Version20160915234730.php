0dbbde2ee39b4d46129830a707ba2eb0da0d9b62
Jessica Hopper <jessica.hopper@gmail.com>
2016-09-15 23:48:46 +0000

    migratex2

---
+++ app/DoctrineMigrations/Version20160915234730.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160915234730 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    $sql = 'INSERT INTO `risk_tolerance` VALUES (2,\'aggressive growth\'),(1,\'growth\')';
+        $this->connection->executeQuery($sql);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
